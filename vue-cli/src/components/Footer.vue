c6639986745daa798d3127558fea61e051be86ef
danbi choi <eks4116@gmail.com>
2019-09-10 17:35:53 +0900

    props, emit test

---
+++ vue-cli/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p>{{ myname }}</p>
+        <p>{{ area.city }}</p>
+        <button @click="changeName">Change</button>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        name:{
+            type: String,
+            default: 'Max'
+        },
+        area:{
+            type: Object,
+            default: function () {
+                return { city : 'Seoul'};
+            }
+        }
+    },
+    data(){
+        return {
+            myname : this.name
+        }
+    },
+    methods: {
+    changeName() {
+        this.myname = 'Danbi';
+        this.$emit('changeName',this.myname);
+    }
+    }
+}
+</script>
